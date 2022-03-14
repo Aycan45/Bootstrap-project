7bcb11019b1a0e7920b98a2f177fcd930fde7af2
Aycan Feyzulla <43782112+Aycan45@users.noreply.github.com>
2022-03-14 15:14:49 +0200

    Bootsrap 5 project from univeristy

---
+++ assets/js/dashboard.js
@@ -0,0 +1,57 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+      ],
+      datasets: [{
+        data: [
+          0,
+          600,
+          400,
+          200,
+          300,
+          100,
+          400,
+          200,
+          600
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#FFFFFF',
+        borderWidth: 4,
+        pointBackgroundColor: '#FFFFFF'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
